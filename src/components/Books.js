b9e928de7bf0668a127c7e3c33395c57455be99a
kennbeckloff <kennbeckloff688@gmail.com>
2022-09-07 13:53:22 +0300

    book page and css

---
+++ src/components/Books.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Books(props) {
+  return (
+    <>
+      <div className="uk-card-media-top">
+        <img
+          src={props.image}
+          width="auto"
+          height="auto"
+          className="uk-object-position-center"
+          alt=""
+        />
+      </div>
+      <div className="uk-card-body" id="book-card">
+        <h3 className="uk-card-title" id="book-title">
+          {props.title}
+        </h3>
+        <p className="book-authors">Author:{props.authors}</p>
+        <p className="uk-panel-scrollable" id="book-description">
+          {props.description}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Books;
